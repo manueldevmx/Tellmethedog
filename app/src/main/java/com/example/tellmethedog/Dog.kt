6fa0768cee9f1c0bab04f20445beed381f3b1ac7
manueldevmx <jhernandezcrux@gmail.com>
2022-06-07 11:57:05 -0500

    add class
    dog & DogList

---
+++ app/src/main/java/com/example/tellmethedog/Dog.kt
@@ -0,0 +1,16 @@
+package com.example.tellmethedog
+
+class Dog (
+    val id: Long,
+    val index: Int,
+    val name: String,
+    val type: String,
+    val heightFemale: Double,
+    val heightMale: Double,
+    val imageUrl: String,
+    val lifeExpectency: String,
+    val temperament: String,
+    val weightFemale: Double,
+    val weightMale: Double
+)
+
